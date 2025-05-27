fad6868889deb2e5622c4ea4856bce0531ad18b0
xgaslann <gorkem_aslan0@hotmail.com>
2025-05-28 00:43:05 +0300

    add dotenv support and application configuration

--- src/main/java/com/xgaslan/springjwt/SpringJwtApplication.java
+++ src/main/java/com/xgaslan/springjwt/SpringJwtApplication.java
@@ -1,5 +1,6 @@
 package com.xgaslan.springjwt;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJwtApplication {
 
     public static void main(String[] args) {
+        Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
         SpringApplication.run(SpringJwtApplication.class, args);
     }
 
