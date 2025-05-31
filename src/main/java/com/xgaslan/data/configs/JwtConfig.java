c88d7ce80bcb68220b9ba97cc1b43d640a2fa357
xgaslann <gorkem_aslan0@hotmail.com>
2025-05-31 16:38:36 +0300

    add JwtConfig and enhance JwtService for token handling
    Introduced `JwtConfig` for managing JWT-related configurations. Enhanced `JwtService` with functionalities such as token generation, extraction, validation, and expiration checks. Updated JWT dependencies to version 0.12.6 in `pom.xml`.

---
+++ src/main/java/com/xgaslan/data/configs/JwtConfig.java
@@ -0,0 +1,20 @@
+package com.xgaslan.data.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
+public class JwtConfig {
+
+    private String secret;
+    private long expirationTimeMillis; // in milliseconds
+    private String issuer;
+    private String audience;
+    private long refreshExpirationTime; // in milliseconds
+
+}
--- src/main/java/com/xgaslan/security/jwt/JwtService.java
+++ src/main/java/com/xgaslan/security/jwt/JwtService.java
@@ -1,15 +1,88 @@
 package com.xgaslan.security.jwt;
 
+import com.xgaslan.data.configs.JwtConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtService {
 
-    // This class can be used to handle JWT operations such as token generation, validation, etc.
-    // For now, it is empty and can be implemented later as per requirements.
+    private final JwtConfig _jwtConfig;
 
-    // Example methods could include:
-    // - generateToken(String username)
-    // - validateToken(String token)
-    // - getUsernameFromToken(String token)
+    public JwtService(JwtConfig jwtConfig) {
+        _jwtConfig = jwtConfig;
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> extraClaims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuer(_jwtConfig.getIssuer())
+                .audience().add(_jwtConfig.getAudience()).and()
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + _jwtConfig.getExpirationTimeMillis()))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public SecretKey getSigningKey() {
+        // Implementation for retrieving the signing key
+        var keyBytes = Decoders.BASE64.decode(_jwtConfig.getSecret());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        // Implementation for extracting claims from the JWT token
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        // Implementation for extracting all claims from the JWT token
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+    public boolean isTokenExpired(String token) {
+        // Implementation for checking if the JWT token is expired
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        // Implementation for extracting expiration date from the JWT token
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        // Implementation for validating the JWT token
+        final String username = extractClaim(token, Claims::getSubject);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String getUsernameFromToken(String token) {
+        // Implementation for extracting username from the JWT token
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String getIssuerFromToken(String token) {
+        // Implementation for extracting issuer from the JWT token
+        return extractClaim(token, Claims::getIssuer);
+    }
+
 }
